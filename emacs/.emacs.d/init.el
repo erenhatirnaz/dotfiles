3f223bb5267f97ea161f79e386698b40c2fbe55e
Eren Hatırnaz <erenhatirnaz@hotmail.com.tr>
2020-08-19 10:34:29 +0300

    emacs: Bye bye Cask. Welcome use-package
    My `configuration.org` file is very very complicated and unorganized. Need to
    refactor. But I don't have enough time for this. So may be future.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,7 +1,29 @@
-(package-initialize)
+;; Add melpa-stable to package-archives
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") :t)
 
-(require 'cask "~/.cask/cask.el")
-(cask-initialize)
+;; Initialise the packages, avoiding a re-initialisation.
+(unless (bound-and-true-p package--initialized)
+  (setq package-enable-at-startup nil)
+  (package-initialize))
+
+;; Make sure `use-package' is available.
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
+
+;; If `configuration.el' file is exists then load the file. Otherwise build and
+;; load the `.el' file from the `.org' file.
+;; (use-package org)
+;; (let* ((conf "~/.emacs.d/configuration")
+;;        (el (concat conf ".el"))
+;;        (org (concat conf ".org")))
+;;   (if (file-exists-p el)
+;;       (load-file el)
+;;     (org-babel-load-file org)))
 
 (org-babel-load-file "~/.emacs.d/configuration.org")
 (custom-set-variables
@@ -9,10 +31,16 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-	 (quote
-		("fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" default)))
  '(package-selected-packages
 	 (quote
-		(edit-indirect yasnippet yaml-mode white-sand-theme which-key use-package unicode-fonts undo-tree typescript-mode sparql-mode spacemacs-theme smex rainbow-delimiters projectile php-mode paredit origami org-cliplink org-bullets multi-term move-text moody markdown-preview-mode magit indent-guide imenu-list htmlize highlight-indent-guides helm gruvbox-theme golden-ratio go-mode gitignore-mode gitconfig-mode flx-ido fill-column-indicator engine-mode elfeed editorconfig doom-modeline diminish diff-hl darktooth-theme company cask avy async-await ag 2048-game)))
+		(company yasnippet yaml-mode which-key use-package undo-tree
+						 treemacs smex rainbow-delimiters paredit org-cliplink
+						 org-bullets move-text magit gruvbox-theme golden-ratio
+						 flx-ido editorconfig diminish diff-hl delight
+						 all-the-icons-dired))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  )
