17025b1470426edc981fa3d5a3fe5c72e4f69a37
Eren Hatırnaz <erenhatirnaz@hotmail.com.tr>
2020-10-06 01:53:11 +0300

    toolbox: removed old python script
    This python script moved into httpserver bash script. Because I want to keep
    everything in a single file as possible to increase portability.

--- toolbox/bin/httpserver.py
+++
@@ -1,9 +0,0 @@
-import SimpleHTTPServer
-
-map = SimpleHTTPServer.SimpleHTTPRequestHandler.extensions_map
-map[""] = "text/plain"
-
-for key, value in map.items():
-	map[key] = value + ";charset=UTF-8"
-
-SimpleHTTPServer.test()
