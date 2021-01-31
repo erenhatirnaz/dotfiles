4699f2b4dc4b53dfe1d2b1c225994ca0aecfed88
Eren Hatırnaz <erenhatirnaz@hotmail.com.tr>
2021-01-31 22:11:56 +0300

    emacs: create 'early-init.el' and ignore 'package-quickstart.el'

---
+++ emacs/.emacs.d/early-init.el
@@ -0,0 +1,12 @@
+;; Initialise installed packages
+(setq package-enable-at-startup t)
+
+;; Allow loading from the package cache.
+(setq package-quickstart t)
+
+;; Do not resize the frame at this early stage.
+(setq frame-inhibit-implied-resize t)
+
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
