5ccee42099e90300581a53106f5c61762605f923
Eren Hatırnaz <erenhatirnaz@hotmail.com.tr>
2021-08-21 20:44:32 +0300

    emacs: moved persistent datas from user-home-directory to .cache dir

--- emacs/.emacs.d/early-init.el
+++ emacs/.emacs.d/early-init.el
@@ -1,8 +1,17 @@
 ;; Initialise installed packages
 (setq package-enable-at-startup t)
 
+;; Set `user-cache-directory' variable
+;; Crate the directory If it's not exists
+(defvar user-cache-directory
+  "~/.cache/emacs/"
+  "The directory where place persistent data files.")
+(unless (file-directory-p user-cache-directory)
+  (make-directory user-cache-directory))
+
 ;; Allow loading from the package cache.
-(setq package-quickstart t)
+(setq package-quickstart t
+      package-quickstart-file (expand-file-name "package-quickstart.el" user-cache-directory))
 
 ;; Do not resize the frame at this early stage.
 (setq frame-inhibit-implied-resize t)
