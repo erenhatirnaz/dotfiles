0bd1907fd0a77226b9e70feb5ae0772e9b05ae7d
Eren Hatırnaz <erenhatirnaz@hotmail.com.tr>
2024-10-08 19:45:51 +0300

    bash: migrate ssh profile manager as a bash function

--- bash/.bashrc.d/10-bash_functions.sh
+++ bash/.bashrc.d/10-bash_functions.sh
@@ -16,30 +16,43 @@ function _cw() {
 }
 complete -o default -F _cw cw
 
-# SSH and SCP for work servers
-# * Use seperate ssh config file for work servers
-# * Pre-warning when connecting to production environment
-# * Tab completion for SSH Hostnames
-function sshw() {
-	if [[ $1 == *"prod"* ]]; then
-		echo -e "\e[1;41mYou're connecting to PRODUCTION server. BE CAREFUL!\e[0m";
-    read -p "Press enter to continue..."
-	fi
+# SSH Profile Manager
+#
+# I prefer to use separate SSH configurations for various reasons (such as
+# separate SSH keys for work and personal usage). Using meta-programming
+# techniques for creating bash functions like ssh, sshw, etc., allows me to
+# dynamically generate SSH functions based on different config files found in the
+# ~/.ssh directory. For each config file in the ~/.ssh directory, a function is
+# created with a suffix derived from the config file's name. For example, if the
+# config file is named 'config-work', a function `sshw` will be created where 'w'
+# stands for work.
+#
+# It offers the following features:
+#  - A warning before connecting to a production server.
+#  - Auto-completion for hosts from each SSH config
+#  - Separate ssh, scp, and sftp functions for each SSH config
+while read -r config; do
+  suffix=$(basename $config | cut -sd'-' -f2 | cut -c1-1)
 
-	ssh -F ~/.ssh/config-work "$@"
-}
-function _sshw() {
-	IFS=$'\n' tmp=($(compgen -W "$(grep "^Host" ~/.ssh/config-work | grep -v "[?*]" | cut -d" " -f2-)" -- "${COMP_WORDS[$COMP_CWORD]}"))
-	COMPREPLY=( "${tmp[@]// /\ }" )
-}
-complete -o default -F _sshw sshw
+source /dev/stdin <<FUNC
+  function ssh$suffix() {
+    if [[ \$1 == *"prod"* ]]; then
+      echo -e "\e[1;41mYou're about to connect to PRODUCTION server. BE CAREFUL!\e[0m";
+      read -p "Press enter to continue..."
+    fi
 
-function scpw() {
-  scp -F ~/.ssh/config-work "$@"
-}
-# TODO: Filename compilation not works when copying from host to remote
-function _scpw() {
-	IFS=$'\n' tmp=($(compgen -W "$(grep "^Host" ~/.ssh/config-work | grep -v "[?*]" | awk '{print $2":"}')" -- "${COMP_WORDS[$COMP_CWORD]}"))
-	COMPREPLY=( "${tmp[@]}" )
-}
-complete -o nospace -F _scpw scpw
+    /usr/bin/ssh -F "$config" "\$@"
+  }
+
+  function _ssh$suffix() {
+    IFS=\$'\n' tmp=(\$(compgen -W "\$(grep "^Host" $config | grep -v "[?*]" | cut -d" " -f2-)" -- "\${COMP_WORDS[\$COMP_CWORD]}"))
+    COMPREPLY=( "\${tmp[@]// /\ }" )
+  }
+FUNC
+
+complete -o "default" \
+         -o "nospace" \
+         -F _ssh$suffix \
+         scp$suffix ssh$suffix sftp$suffix
+done < <(find "${HOME}/.ssh/" -name "config*")
+unset suffix
