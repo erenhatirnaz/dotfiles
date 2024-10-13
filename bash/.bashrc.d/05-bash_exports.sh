dc84f5509a16068973287295359f3236de76f71b
Eren Hatırnaz <erenhatirnaz@hotmail.com.tr>
2024-10-13 17:26:27 +0300

    bash: update exports
    - added xdg defaults.
    - added directory creation for `$XDG_STATE_HOME`.
    - moved bash and node history file from home directory inside to
      `$XDG_STATE_HOME`.
    - `$NODE_REPL_HISTORY_SIZE` is now uses `$HISTSIZE`.
    - refactor $PATH variable for multi os compatibility.

--- bash/.bashrc.d/05-bash_exports.sh
+++ bash/.bashrc.d/05-bash_exports.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 
+# XDG Defaults
+export XDG_CONFIG_HOME="${HOME}/.config/"
+export XDG_CACHE_HOME="${HOME}/.cache/"
+export XDG_DATA_HOME="${HOME}/.local/share/"
+export XDG_STATE_HOME="${HOME}/.local/state/"
+
+# Create directories for states
+STATE_DIRS=("bash" "node")
+for directory in "${STATE_DIRS[@]}"; do
+  mkdir -p "${XDG_STATE_HOME}/${directory}";
+done
+
 # Make emacs the default editor.
 export EDITOR='emacsclient -c';
-
-# Enable persistent REPL history for `node`.
-export NODE_REPL_HISTORY=~/.node_history;
-# Allow 32³ entries; the default is 1000.
-export NODE_REPL_HISTORY_SIZE='32768';
-# Use sloppy mode by default, matching web browsers.
-export NODE_REPL_MODE='sloppy';
-
-# Make Python use UTF-8 encoding for output to stdin, stdout, and stderr.
-export PYTHONIOENCODING='UTF-8';
 
 # Increase Bash history size. Allow 32³ entries; the default is 500.
 export HISTSIZE='32768';
@@ -20,6 +22,18 @@ export HISTFILESIZE="${HISTSIZE}";
 export HISTCONTROL='ignoreboth';
 # Add date time info to history file
 export HISTTIMEFORMAT="%h %d %H:%M:%S "
+# Change Bash history file path
+export HISTFILE="${XDG_STATE_HOME}/bash/history"
+
+# Enable persistent REPL history for `node`.
+export NODE_REPL_HISTORY="${XDG_STATE_HOME}/node/history";
+# Allow 32³ entries; the default is 1000.
+export NODE_REPL_HISTORY_SIZE="${HISTSIZE}";
+# Use sloppy mode by default, matching web browsers.
+export NODE_REPL_MODE='sloppy';
+
+# Make Python use UTF-8 encoding for output to stdin, stdout, and stderr.
+export PYTHONIOENCODING='UTF-8';
 
 # Set term style
 export TERM="xterm-256color"
@@ -35,22 +49,36 @@ export CARGO_HOME=${HOME}/.cargo
 
 # Set java directory
 export _JAVA_OPTIONS=-Djava.util.prefs.userRoot="$XDG_CONFIG_HOME"/java
+if [[ "$OSTYPE" =~ "darwin"* ]]; then
+  export CPPFLAGS="-I/opt/homebrew/opt/openjdk/include"
+fi
 
 # Set GNU readline inputrc path
 export INPUTRC="${HOME}/.bashrc.d/15-inputrc.conf"
 
-# My scripts
-export PATH=${PATH}:${HOME}/bin
+# PATH variable for GNU/Linux
+if [[ "${OSTYPE}" =~ "linux-gnu" ]]; then
+  export PATH=${PATH}:${HOME}/.local/bin
+  export PATH=${PATH}:${HOME}/.npm/bin
+  export PATH=${PATH}:${HOME}/.yarn/bin
+  export PATH=${PATH}:${HOME}/.cask/bin
+  export PATH=${PATH}:${HOME}/.cargo/bin
+  export PATH=${PATH}:${HOME}/.rvm/bin
+  export PATH=${PATH}:${HOME}/.nimble/bin
+  export PATH=${PATH}:${HOME}/.config/composer/vendor/bin
+  export PATH=${PATH}:${HOME}/.fzf/bin
+  export PATH=${PATH}:${HOME}/go/bin
+  export PATH=${PATH}:${HOME}/.local/share/gem/ruby/3.0.0/bin
+fi
 
-# Add 3th party tools to PATH variable
-export PATH=${PATH}:${HOME}/.local/bin
-export PATH=${PATH}:${HOME}/.npm/bin
-export PATH=${PATH}:${HOME}/.yarn/bin
-export PATH=${PATH}:${HOME}/.cask/bin
-export PATH=${PATH}:${HOME}/.cargo/bin
-export PATH=${PATH}:${HOME}/.rvm/bin
-export PATH=${PATH}:${HOME}/.nimble/bin
-export PATH=${PATH}:${HOME}/.config/composer/vendor/bin
-export PATH=${PATH}:${HOME}/.fzf/bin
-export PATH=${PATH}:${HOME}/go/bin
-export PATH=${PATH}:${HOME}/.local/share/gem/ruby/3.0.0/bin
+# PATH variable for MacOS
+if [[ "$OSTYPE" =~ "darwin"* ]]; then
+  export PATH=/opt/homebrew/opt/coreutils/libexec/gnubin:${PATH}
+  export PATH=/opt/homebrew/opt/findutils/libexec/gnubin:${PATH}
+  export PATH=/opt/homebrew/opt/gnu-indent/libexec/gnubin:${PATH}
+  export PATH=/opt/homebrew/opt/gnu-sed/libexec/gnubin:${PATH}
+  export PATH=/opt/homebrew/opt/gnu-tar/libexec/gnubin:${PATH}
+  export PATH=/opt/homebrew/opt/grep/libexec/gnubin:${PATH}
+  export PATH=/opt/homebrew/opt/gawk/libexec/gnubin:${PATH}
+  export PATH=/opt/homebrew/bin:${PATH}
+fi
